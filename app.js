4ba5d775136ad53a360eee2eac7a96684d8bebc2
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-11-16 09:25:57 -0800

    a8 4.3

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 const app = express();
+app.use(express.json());
 HelloController(app);
 UserController(app);
 app.listen(4000);
--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -4,6 +4,7 @@ let users = people;
 const UserController = (app) => {
   app.get("/api/users", findUsers);
   app.get("/api/users/:uid", findUserById);
+  app.post("/api/users", createUser);
 };
 
 const findUsers = (req, res) => {
@@ -20,6 +21,13 @@ const findUserById = (req, res) => {
   const userId = req.params.uid;
   const user = users.find((u) => u._id === userId);
   res.json(user);
+};
+
+const createUser = (req, res) => {
+  const newUser = req.body;
+  newUser._id = new Date().getTime() + "";
+  users.push(newUser);
+  res.json(newUser);
 };
 
 export default UserController;
