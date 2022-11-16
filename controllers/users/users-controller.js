4a7feef7560ddee7fd4825d824c4361bb97a3004
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-11-15 21:10:42 -0800

    a8 3.3

--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -3,6 +3,7 @@ let users = people;
 
 const UserController = (app) => {
   app.get("/api/users", findUsers);
+  app.get("/api/users/:uid", findUserById);
 };
 
 const findUsers = (req, res) => {
@@ -13,6 +14,12 @@ const findUsers = (req, res) => {
     return;
   }
   res.json(users);
+};
+
+const findUserById = (req, res) => {
+  const userId = req.params.uid;
+  const user = users.find((u) => u._id === userId);
+  res.json(user);
 };
 
 export default UserController;
