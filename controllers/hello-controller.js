0edba678c6673f65312b5a7463e1c8eaa62dc24a
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-11-15 18:49:32 -0800

    a8 3.1

--- controllers/hello-controller.js
+++ controllers/hello-controller.js
@@ -1,6 +1,9 @@
-app.get("/hello", (req, res) => {
-  res.send("Life is good!");
-});
-app.get("/", (req, res) => {
-  res.send("Welcome to Full Stack Development!");
-});
+const HelloController = (app) => {
+  app.get("/hello", (req, res) => {
+    res.send("Life is good!");
+  });
+  app.get("/", (req, res) => {
+    res.send("Welcome to Full Stack Development!");
+  });
+};
+export default HelloController;
