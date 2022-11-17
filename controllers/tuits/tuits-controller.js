6504dfc35f785a6523ce014e8fe7953ce13becb9
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-11-17 09:17:14 -0800

    a8 5.1

---
+++ controllers/tuits/tuits-controller.js
@@ -0,0 +1,16 @@
+import posts from "./tuits.js";
+let tuits = posts;
+
+const createTuit = (req, res) => {};
+const findTuits = (req, res) => {
+  res.json(tuits);
+};
+const updateTuit = (req, res) => {};
+const deleteTuit = (req, res) => {};
+
+export default (app) => {
+  app.post("/api/tuits", createTuit);
+  app.get("/api/tuits", findTuits);
+  app.put("/api/tuits/:tid", updateTuit);
+  app.delete("/api/tuits/:tid", deleteTuit);
+};
