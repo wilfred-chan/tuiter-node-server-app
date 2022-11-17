1d6ba011a5c0ce3d6df54c6b5c7d5587d6e4e9b5
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-11-17 10:51:15 -0800

    a8 6

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 HelloController(app);
